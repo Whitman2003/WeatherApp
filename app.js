ad168fc9a7f7c7216af761dbdb04603b4fb62918
Whitman2003 <whitmanaidan@yahoo.com>
2025-02-21 09:42:09 -0800

    Changing to Daily

--- app.js
+++ app.js
@@ -28,30 +28,31 @@ document.querySelector('#btnLocation').addEventListener('click', async function(
         return;
     }
 
-    const weatherURL =`https://api.open-meteo.com/v1/forecast?latitude=${strLat}&longitude=${strLong}&daily=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,cloud_cover,visibility,wind_speed_10m,wind_direction_10m,wind_gusts_10m,soil_temperature_0cm,soil_moisture_0_to_1cm`;
+    const weatherURL =`https://api.open-meteo.com/v1/forecast?latitude=${strLat}&longitude=${strLong}&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration`;
       
     //Gets the data
     try {
         console.log("API URL: ", weatherURL);
-        const objWeatherResonse = await fetch(weatherURL);
-        const objWeatherResonses = await objWeatherResonse.json();
+        const objWeatherResponse = await fetch(weatherURL);
+        const objWeatherResponses = await objWeatherResponse.json();
+        console.log(objWeatherResponses);
 
-        console.log("API Response: ", objWeatherResonses);
+        console.log("API Response: ", objWeatherResponses);
 
-        if (!objWeatherResonse || objWeatherResonse.length === 0) {
+        if (!objWeatherResponse || objWeatherResponse.length === 0) {
             throw new Error("No data returned from the API");
         }
 
-        console.log("Response: ", objWeatherResonse);
+        console.log("Response: ", objWeatherResponse);
         
         //Set the data
-        const strLatitude = objWeatherResonses.latitude;
-        const strLongitude = objWeatherResonses.longitude;
-        const strGenerationTime = objWeatherResonses.generationtime_ms;
-        const strUTCOffsetSeconds = objWeatherResonses.utc_offset_seconds;
-        const strTimezone = objWeatherResonses.timezone;
-        const strTimezoneAbbr = objWeatherResonses.timezone_abbreviation;
-        const strEleveation = objWeatherResonses.elevation;
+        const strLatitude = objWeatherResponses.latitude;
+        const strLongitude = objWeatherResponses.longitude;
+        const strGenerationTime = objWeatherResponses.generationtime_ms;
+        const strUTCOffsetSeconds = objWeatherResponses.utc_offset_seconds;
+        const strTimezone = objWeatherResponses.timezone;
+        const strTimezoneAbbr = objWeatherResponses.timezone_abbreviation;
+        const strEleveation = objWeatherResponses.elevation;
 
         //Make into HTML
         const confirmWeatherDataHTML = `
@@ -66,35 +67,35 @@ document.querySelector('#btnLocation').addEventListener('click', async function(
         document.querySelector('#confirmWeatherData').innerHTML = confirmWeatherDataHTML;
 
         //Set the hourly units
-        const strTime = objWeatherResonses.hourly_units.time;
-        const strTemperature = objWeatherResonses.hourly_units.temperature_2m;
-        const strRelativeHumidity = objWeatherResonses.hourly_units.relative_humidity_2m;
-        const strApparentTemperature = objWeatherResonses.hourly_units.apparent_temperature;
-        const strPrecipitationProbability = objWeatherResonses.hourly_units.precipitation_probability;
-        const strPrecipitation = objWeatherResonses.hourly_units.precipitation;
-        const strRain = objWeatherResonses.hourly_units.rain;
-        const strShowers = objWeatherResonses.hourly_units.showers;
-        const strSnowfall = objWeatherResonses.hourly_units.snowfall;
-        const strSnowDepth = objWeatherResonses.hourly_units.snow_depth;
-        const strCloudCover = objWeatherResonses.hourly_units.cloud_cover;
-        const strVisibility = objWeatherResonses.hourly_units.visibility;
-        const strWindSpeed = objWeatherResonses.hourly_units.wind_speed_10m;
-        const strWindDirection = objWeatherResonses.hourly_units.wind_direction_10m;
-        const strWindGusts = objWeatherResonses.hourly_units.wind_gusts_10m;
-        const strSoilTemperature = objWeatherResonses.hourly_units.soil_temperature_0cm;
-        const strSoilMoisture = objWeatherResonses.hourly_units.soil_moisture_0_to_1cm;
+        const strTime = objWeatherResponses.hourly_units.time;
+        const strTemperature = objWeatherResponses.hourly_units.temperature_2m;
+        const strRelativeHumidity = objWeatherResponses.hourly_units.relative_humidity_2m;
+        const strApparentTemperature = objWeatherResponses.hourly_units.apparent_temperature;
+        const strPrecipitationProbability = objWeatherResponses.hourly_units.precipitation_probability;
+        const strPrecipitation = objWeatherResponses.hourly_units.precipitation;
+        const strRain = objWeatherResponses.hourly_units.rain;
+        const strShowers = objWeatherResponses.hourly_units.showers;
+        const strSnowfall = objWeatherResponses.hourly_units.snowfall;
+        const strSnowDepth = objWeatherResponses.hourly_units.snow_depth;
+        const strCloudCover = objWeatherResponses.hourly_units.cloud_cover;
+        const strVisibility = objWeatherResponses.hourly_units.visibility;
+        const strWindSpeed = objWeatherResponses.hourly_units.wind_speed_10m;
+        const strWindDirection = objWeatherResponses.hourly_units.wind_direction_10m;
+        const strWindGusts = objWeatherResponses.hourly_units.wind_gusts_10m;
+        const strSoilTemperature = objWeatherResponses.hourly_units.soil_temperature_0cm;
+        const strSoilMoisture = objWeatherResponses.hourly_units.soil_moisture_0_to_1cm;
 
         //Set the hourly data
         //Time for all
-        const arrTime = objWeatherResonses.hourly.time;
+        const arrTime = objWeatherResponses.hourly.time;
 
         //Precipitation
-        const arrPrecipitationProbability = objWeatherResonses.hourly.precipitation_probability;
-        const arrPrecipitation = objWeatherResonses.hourly.precipitation;
-        const arrRain = objWeatherResonses.hourly.rain;
-        const arrShowers = objWeatherResonses.hourly.showers;
-        const arrSnowfall = objWeatherResonses.hourly.snowfall;
-        const arrSnowDepth = objWeatherResonses.hourly.snow_depth;
+        const arrPrecipitationProbability = objWeatherResponses.hourly.precipitation_probability;
+        const arrPrecipitation = objWeatherResponses.hourly.precipitation;
+        const arrRain = objWeatherResponses.hourly.rain;
+        const arrShowers = objWeatherResponses.hourly.showers;
+        const arrSnowfall = objWeatherResponses.hourly.snowfall;
+        const arrSnowDepth = objWeatherResponses.hourly.snow_depth;
 
         //Make into HTML
         let hourlyPrecipitationDataHTML = '<table class="weather-table"><thread><tr><th>Time</th><th>Precipitation Probability</th><th>Precipitation</th><th>Rain</th><th>Showers</th><th>Snowfall</th><th>Snow Depth</th></tr></thread><tbody>';
@@ -145,9 +146,9 @@ document.querySelector('#btnLocation').addEventListener('click', async function(
         precipitationCtx.fillText('Precipitation (mm)', 10, 20);
 
         //Wind
-        const arrWindSpeed = objWeatherResonses.hourly.wind_speed_10m;
-        const arrWindDirection = objWeatherResonses.hourly.wind_direction_10m;
-        const arrWindGusts = objWeatherResonses.hourly.wind_gusts_10m;
+        const arrWindSpeed = objWeatherResponses.hourly.wind_speed_10m;
+        const arrWindDirection = objWeatherResponses.hourly.wind_direction_10m;
+        const arrWindGusts = objWeatherResponses.hourly.wind_gusts_10m;
 
         //Make into HTML
         let hourlyWindDataHTML = '<table class="weather-table"><thread><tr><th>Time</th><th>Wind Speed</th><th>Wind Direction</th><th>Wind Gusts</th></tr></thread><tbody>';
@@ -164,9 +165,9 @@ document.querySelector('#btnLocation').addEventListener('click', async function(
         document.querySelector('#hourlyWindData').innerHTML = hourlyWindDataHTML;
 
         //Temperature
-        const arrTemperature = objWeatherResonses.hourly.temperature_2m;
-        const arrRelativeHumidity = objWeatherResonses.hourly.relative_humidity_2m;
-        const arrApparentTemperature = objWeatherResonses.hourly.apparent_temperature;
+        const arrTemperature = objWeatherResponses.hourly.temperature_2m;
+        const arrRelativeHumidity = objWeatherResponses.hourly.relative_humidity_2m;
+        const arrApparentTemperature = objWeatherResponses.hourly.apparent_temperature;
 
         //Make into HTML
         let hourlyTemperatureDataHTML = '<table class="weather-table"><thread><tr><th>Time</th><th>Temperature</th><th>Relative Humidity</th><th>Apparent Temperature</th></tr></thread><tbody>';
@@ -183,8 +184,8 @@ document.querySelector('#btnLocation').addEventListener('click', async function(
         document.querySelector('#hourlyTemperatureData').innerHTML = hourlyTemperatureDataHTML;
         
         //Visibility
-        const arrCloudCover = objWeatherResonses.hourly.cloud_cover;
-        const arrVisibility = objWeatherResonses.hourly.visibility;
+        const arrCloudCover = objWeatherResponses.hourly.cloud_cover;
+        const arrVisibility = objWeatherResponses.hourly.visibility;
 
         //Make into HTML
         let hourlyVisibilityDataHTML = '<table class="weather-table"><thread><tr><th>Time</th><th>Cloud Coverage</th><th>Visibility</th></tr></thread><tbody>';
@@ -200,8 +201,8 @@ document.querySelector('#btnLocation').addEventListener('click', async function(
         document.querySelector('#hourlyVisibilityData').innerHTML = hourlyVisibilityDataHTML;
         
         //Soil
-        const arrSoilTemperature = objWeatherResonses.hourly.soil_temperature_0cm;
-        const arrSoilMoisture = objWeatherResonses.hourly.soil_moisture_0_to_1cm;
+        const arrSoilTemperature = objWeatherResponses.hourly.soil_temperature_0cm;
+        const arrSoilMoisture = objWeatherResponses.hourly.soil_moisture_0_to_1cm;
 
         //Make into HTML
         let hourlySoilDataHTML = '<table class="weather-table"><thread><tr><th>Time</th><th>Soil Temperature</th><th>Soil Moisture</th></tr></thread><tbody>';
